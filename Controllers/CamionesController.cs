24cc747b65f2a9cd1bd2cd90d817907b036d78ea
Tifany <tifanyadd8@gmail.com>
2024-11-19 11:01:37 -0600

    Camiones insertar terminado

--- Controllers/CamionesController.cs
+++ Controllers/CamionesController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using DTO;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using Transportes_MVC.Models;
@@ -15,10 +18,10 @@ namespace Transportes_MVC.Controllers
             //creo ena lista de camiones del modelo Original
             List<Camiones> lista_camiones = new List<Camiones>();
             //lleno la lista con elementos existentes dentro del contexto(DB) utilizando EF y LinQ
-            using(TransportesEntities context = new TransportesEntities())
+            using (TransportesEntities context = new TransportesEntities())
             {
                 //llenos mi lista directamente usando linQ
-                lista_camiones =(from c in context.Camiones select c).ToList();
+                lista_camiones = (from c in context.Camiones select c).ToList();
                 //// otra forma de hacer lo mismo 
                 //lista_camiones = context.Camiones.ToList();
                 ////otra forma de hacer lo mismo
@@ -41,5 +44,95 @@ namespace Transportes_MVC.Controllers
             //retorno la vista con los datos del modelo
             return View(lista_camiones);
         }
+        //GET: NUevo_Camion
+        public ActionResult Nuevo_Camion()
+        {
+            ViewBag.Titulo = "Nuevo Camión";
+            //cargar DLL con las opciones del tipo de camión
+            cargarDDL();
+            return View();
+        }
+        //POST: Nuevo_Camion
+        [HttpPost]
+        public ActionResult Nuevo_Camion(Camiones_DTO model, HttpPostedFileBase imagen)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (TransportesEntities contex = new TransportesEntities()) //creo una instancia de un solo uso de mi contexto
+                    {
+                        //creo un objeto basado en el contexto(Modelo Original)
+                        var camion = new Camiones();
+
+                        //asigno todos los valores del modelo de entrada (DTO) el objeto que será insertado en la BD(model original)
+                        camion.Matricula = model.Matricula;
+                        camion.Tipo_Camion = model.Tipo_Camion;
+                        camion.Marca = model.Marca;
+                        camion.Modelo = model.Modelo;
+                        camion.Capacidad = model.Capacidad;
+                        camion.Kilometraje = model.Kilometraje;
+                        camion.Disponibilidad = model.Disponibilidad;
+
+                        //valido si existe una IMG o no
+                        if (imagen != null && imagen.ContentLength > 0)
+                        {
+                            string filename = Path.GetFileName(imagen.FileName); //recupero el nombre de mi archivo
+                            string pathdir = Server.MapPath("~/Assets/Imagenes/Camiones/"); //mapeo la ruta donde guardare mis imagenes en el servidor
+                            if (!Directory.Exists(pathdir)) // si no existe el directorio, lo creo
+                            {
+                                Directory.CreateDirectory(pathdir);
+                            }
+                            imagen.SaveAs(pathdir + filename); //guardo la imagen en el servidor
+                            camion.UrlFoto = "/Assets/Imagenes/Camiones/" + filename; //guardo la ruta y el nombre del archivo en el camion que voy a insertar
+
+                            //Impacto dobre la BD usando EF
+                            contex.Camiones.Add(camion); //agrego un nuevo camion al contexto
+                            contex.SaveChanges(); //Impactar la bd (enviar los cambios entre el contexto y la bd)
+                            //sweet alert
+                            return RedirectToAction("Index"); //finalmente, regreso al listado si es que todo salio bien
+                        }
+                        else
+                        {
+                            //Sweet alert
+                            cargarDDL();
+                            return View(model);
+                        }
+                    }
+                }
+                else
+                {
+                    //En este caso que ocurra alguna exepcion, voy a mostrar un msj con el error, voy a volver a cargar la lista de opciones dek tipo_camion(cargarDDL) para que no marque error el formulario, y devuelve la misma vista con los mismos datos que me han sido enviados
+                    //Sweet alert
+                    cargarDDL();
+                    return View(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                //En este caso que ocurra alguna exepcion, voy a mostrar un msj con el error, voy a volver a cargar la lista de opciones dek tipo_camion(cargarDDL) para que no marque error el formulario, y devuelve la misma vista con los mismos datos que me han sido enviados
+                //Sweet alert
+                cargarDDL();
+                return View(model);
+            }
+        }
+        #region Auxiliares
+        private class Opciones
+        {
+            public string Numero { get; set; }
+            public string Descripcion { get; set; }
+        }
+        public void cargarDDL()
+        {
+            List<Opciones> lista_opciones = new List<Opciones>()
+            {
+                new Opciones(){Numero = "0", Descripcion="Seleccione un opción"},
+                new Opciones(){Numero = "1", Descripcion="Volteo"},
+                new Opciones(){Numero = "2", Descripcion="Rodillas"},
+                new Opciones(){Numero = "3", Descripcion="Transporte"}
+            };
+            ViewBag.ListaTipos = lista_opciones;
+        }
+        #endregion
     }
 }
---
+++ DTO/Camiones_DTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Camiones_DTO
+    {
+        //DTO => Data Transfer Object
+        //
+        //
+        [Key] //data annotation
+        public int ID_Camion { get; set; }
+        [Required] //DAta Annotation
+        [Display(Name = "Matrícula")] //DataHelper
+        public string Matricula { get; set; }
+        [Required] //DAta Annotation
+        [Display(Name = "Tipo_Camion")] //DataHelper
+        public string Tipo_Camion { get; set; }
+        [Required] //DAta Annotation
+        [Display(Name = "Marca")] //DataHelper
+        public string Marca { get; set; }
+        [Required] //DAta Annotation
+        [Display(Name = "Modelo")] //DataHelper
+        public string Modelo { get; set; }
+        [Required] //DAta Annotation
+        [Display(Name = "Capacidad")] //DataHelper
+        public int Capacidad { get; set; }
+        [Required] //DAta Annotation
+        [Display(Name = "Kilometraje")] //DataHelper
+        public double Kilometraje { get; set; }
+
+        [DataType(DataType.ImageUrl)] //DataHelper
+        public string UrlFoto { get; set; }
+        [Required] //DAta Annotation
+        [Display(Name = "Disponibilidad")] //DataHelper
+        public bool Disponibilidad { get; set; }
+    }
+}
