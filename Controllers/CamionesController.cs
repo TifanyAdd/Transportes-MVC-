bf457d4d907b60de4203d427be0fe52c8ce657e1
Tifany <tifanyadd8@gmail.com>
2024-11-19 13:25:02 -0600

    Editar camion Completo

--- Controllers/CamionesController.cs
+++ Controllers/CamionesController.cs
@@ -1,8 +1,10 @@
 ﻿using DTO;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
@@ -116,6 +118,198 @@ namespace Transportes_MVC.Controllers
                 return View(model);
             }
         }
+        
+        //GET: Editar_Camion/{id}
+
+        public ActionResult Editar_Camion(int id)
+        {
+            if (id > 0) //validar que realmente llega un id
+            {
+                Camiones_DTO camion = new Camiones_DTO();
+                using (TransportesEntities contex = new TransportesEntities()) //creo una instancia de un unico uso de mi contexto
+                {
+                    //busco aquel camion que tenga el ID
+                    //Bajo metodo
+                    var camion_aux = contex.Camiones.Where(x => x.ID_Camion == id).FirstOrDefault();
+                    camion.ID_Camion = camion_aux.ID_Camion;
+                    camion.Matricula = camion_aux.Matricula;
+                    camion.Marca = camion_aux.Marca;
+                    camion.Modelo = camion_aux.Modelo;
+                    camion.Tipo_Camion = camion_aux.Tipo_Camion;
+                    camion.Capacidad = camion_aux.Capacidad;
+                    camion.Kilometraje = camion_aux.Kilometraje;
+                    camion.Disponibilidad = camion_aux.Disponibilidad;
+                    camion.UrlFoto = camion_aux.UrlFoto;
+
+                    //Bajo consulta
+
+                        camion = (from c in contex.Camiones
+                                  where c.ID_Camion == id
+                                  select new Camiones_DTO()
+                                  {
+                                      ID_Camion = c.ID_Camion,
+                                      Matricula = c.Matricula,
+                                      Marca = c.Marca,
+                                      Modelo = c.Modelo,
+                                      Tipo_Camion = c.Tipo_Camion,
+                                      Capacidad = c. Capacidad,
+                                      Kilometraje = c. Kilometraje,
+                                      Disponibilidad = c.Disponibilidad,
+                                      UrlFoto = c.UrlFoto
+                                  }).FirstOrDefault();
+                } //cierre de using (contex)
+
+                if (camion != null)
+                {
+                    ViewBag.Titulo = "Editar camion" + camion.ID_Camion;
+                    cargarDDL();
+                    return View(camion);
+                }
+                else
+                {
+                    //sweet alert
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                //Sweet Alert
+                return RedirectToAction("Index");
+            }
+        }
+
+        //POST: Nuevo_Camion
+        [HttpPost]
+        public ActionResult Editar_Camion(Camiones_DTO model, HttpPostedFileBase imagen)
+        {
+            try
+            {
+                if (ModelState.IsValid) //Verificar si los campos y los tipos de datos corresponden al Modelo (DTO)
+                {
+                    using (TransportesEntities context = new TransportesEntities()) //creo una isntancia de un único uso de mi Contexto
+                    {
+                        //creo un objeto basado en el contexto (modelo original)
+                        var camion = new Camiones();
+
+                        //asignos los valores del modelos de entrada (DTO) al objeto que se insertará en la BD (Modelo Original)
+                        camion.ID_Camion = model.ID_Camion;
+                        camion.Matricula = model.Matricula;
+                        camion.Marca = model.Marca;
+                        camion.Modelo = model.Modelo;
+                        camion.Capacidad = model.Capacidad;
+                        camion.Tipo_Camion = model.Tipo_Camion;
+                        camion.Disponibilidad = model.Disponibilidad;
+                        camion.Kilometraje = model.Kilometraje;
+
+                        //valido si exite una IMG
+                        if (imagen != null && imagen.ContentLength > 0)
+                        {
+                            string filename = Path.GetFileName(imagen.FileName);
+                            string pathdir = Server.MapPath("~/Assets/Imagenes/Camiones/");
+                            //validar si tiene una foto o no
+                            if (model.UrlFoto.Length == 0)
+                            {
+                                //la imagen en la BD es null y hay que darle la imagen
+                                if (!Directory.Exists(pathdir))
+                                {
+                                    Directory.CreateDirectory(pathdir);
+                                }
+
+                                imagen.SaveAs(pathdir + filename);
+                                camion.UrlFoto = "/Assets/Imagenes/Camiones/" + filename;
+                            }
+                            else
+                            {
+                                //validar si es la misma o es nueva
+                                if (model.UrlFoto.Contains(filename))
+                                {
+                                    //El la misma
+                                    camion.UrlFoto = "/Assets/Imagenes/Camiones/" + filename;
+                                }
+                                else
+                                {
+                                    //es diferente
+                                    if (!Directory.Exists(pathdir))
+                                    {
+                                        Directory.CreateDirectory(pathdir);
+                                    }
+
+                                    //Borro la imagen anterios
+                                    //valido si existe
+
+                                    try
+                                    {
+                                        string pathdir_old = Server.MapPath("~" + model.UrlFoto); //busco la imagen que catualmente tiene el camión
+                                        if (System.IO.File.Exists(pathdir_old)) //valido si existe dicho archivo
+                                        {
+                                            //procedo a eliminarlo
+                                            System.IO.File.Delete(pathdir_old);
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        //Sweet Alert
+                                    }
+
+                                    imagen.SaveAs(pathdir + filename);
+                                    camion.UrlFoto = "/Assets/Imagenes/Camiones/" + filename;
+                                }
+                            }
+
+                        }
+                        else //si no hya nueva imagen, paso la misma
+                        {
+                            camion.UrlFoto = model.UrlFoto;
+                        }
+
+                        //Guardar cambios, validar excepciones, redirigir
+                        //actualizar el estado de nuestro elemento
+                        //.Entry() registrar la entrada de nueva información al contexto y notificar un cambio de estado usando System.Data.Entity.EntityState.Modified
+                        context.Entry(camion).State = System.Data.Entity.EntityState.Modified;
+                        //impactamos la BD
+                        try
+                        {
+                            context.SaveChanges();
+                        }
+                        //agregar using desde 'using System.Data.Entity.Validation;'
+                        catch (DbEntityValidationException ex)
+                        {
+                            string resp = "";
+                            //recorro todos los posibles errores de la Entidad Referencial
+                            foreach (var error in ex.EntityValidationErrors)
+                            {
+                                //recorro los detalles de cada error
+                                foreach (var validationError in error.ValidationErrors)
+                                {
+                                    resp += "Error en la Entidad: " + error.Entry.Entity.GetType().Name;
+                                    resp += validationError.PropertyName;
+                                    resp += validationError.ErrorMessage;
+                                }
+                            }
+                            //Sweet Alert
+                        }
+                        //Sweet Alert
+                        return RedirectToAction("Index");
+                    }
+                }
+                else
+                {
+                    //En caso de que ocurra alguna exepción, voy a mostrar un msj con el error, voy a volver a cargar la lista de opciones del tipo_camion (cargarDDL) par que no marque error el formulario, y devulveré la misma vista con los mismos datos que me han sido enviado (vas pa' tras)
+                    //Sweet Alert
+                    cargarDDL();
+                    return View(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                //En caso de que ocurra alguna exepción, voy a mostrar un msj con el error, voy a volver a cargar la lista de opciones del tipo_camion (cargarDDL) par que no marque error el formulario, y devulveré la misma vista con los mismos datos que me han sido enviado (vas pa' tras)
+                //Sweet Alert
+                cargarDDL();
+                return View(model);
+            }
+        }
+
+
         #region Auxiliares
         private class Opciones
         {
